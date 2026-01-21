e3e1500cc76eb1ca82d213934795d60c1717c2a5
Tomerson333 <coca.cola990@spoko.pl>
2026-01-21 13:22:55 +0100

    Dopracowanie formularza i walidacj

--- LibraryManagementSystem/Controllers/AuthorsController.cs
+++ LibraryManagementSystem/Controllers/AuthorsController.cs
@@ -51,7 +51,7 @@ namespace LibraryManagementSystem.Controllers
         {
             return View();
         }
-
+        
         // POST: Authors/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
